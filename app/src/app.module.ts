3c8bc13dffe9f4028601cacdc8ecedf921e27745
Matthew Arcila <arcilamatt@gmail.com>
2022-04-02 21:46:13 -0400

    remove cats module

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -4,32 +4,26 @@ import {
   NestModule,
   RequestMethod,
 } from '@nestjs/common';
-import { CatsController } from './cats/cats.controller';
-import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
-import { BlockController } from './block/block.controller';
+import { BlockController } from './block/controllers/block.controller';
 import { BlockModule } from './block/block.module';
 import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [
-    CatsModule,
-    BlockModule,
-    MongooseModule.forRoot(process.env.MONGO_URI),
-  ],
+  imports: [BlockModule, MongooseModule.forRoot(process.env.MONGO_URI)],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('cats');
+    consumer.apply(LoggerMiddleware).forRoutes('blocks');
     consumer
       .apply(LoggerMiddleware)
-      .forRoutes({ path: 'cats', method: RequestMethod.GET });
+      .forRoutes({ path: 'blocks', method: RequestMethod.GET });
     consumer
       .apply(LoggerMiddleware)
       .exclude(
-        { path: 'cats', method: RequestMethod.GET },
-        { path: 'cats', method: RequestMethod.POST },
-        'cats/(.*)',
+        { path: 'blocks', method: RequestMethod.GET },
+        { path: 'blocks', method: RequestMethod.POST },
+        'blocks/(.*)',
       )
-      .forRoutes(CatsController);
+      .forRoutes(BlockController);
   }
 }
